4d3805442820c8ea93f871925a8a6f5ae1efce6b
Dipti Choubey <diptichoubey101@gmail.com>
2026-02-11 22:26:08 +0530

    Add files via upload

---
+++ operators.kt
@@ -0,0 +1,20 @@
+fun main() {
+    val isActive = true
+    val score = 88
+    val internetSpeed = 4000
+
+    if (isActive && score == 88 && internetSpeed == 4000) {
+        println("you are next level")
+    } else {
+        println("you are same level")
+    }
+
+    val num1 = 5
+    val num2 = 4
+
+    if (num1 > 0 || num2 > 3) {
+        println("true")
+    } else {
+        println("not true")
+    }
+}
