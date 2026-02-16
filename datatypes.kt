0c9b0f591ef2a112a3d6ae3aa3b4fc3ba42cefc2
Dipti Choubey <diptichoubey101@gmail.com>
2026-02-16 20:13:23 +0530

    Add files via upload

---
+++ datatypes.kt
@@ -0,0 +1,23 @@
+fun main() {
+    var age=20
+    var integerMaxValue=Int.MAX_VALUE
+    var integerMinValue=Int.MIN_VALUE
+
+    println("Integer max value is:$integerMaxValue")
+    println("Integer Min value is:$integerMinValue")
+
+    var byteMaxValue=Byte.MAX_VALUE
+    var byteMinValue=Byte.MIN_VALUE
+    println("Byte max value is:$byteMaxValue")
+    println("Byte Min value is:$byteMinValue")
+
+    var shortMaxValue=Short.MAX_VALUE
+    var shortMinValue=Short.MIN_VALUE
+    println("Short max value is:$shortMaxValue")
+    println("Short Min value is:$shortMinValue")
+
+    var longMaxValue=Long.MAX_VALUE
+    var longMinValue=Long.MIN_VALUE
+    println("Long max value is:$longMaxValue")
+    println("Long Min value is:$longMinValue")
+}
