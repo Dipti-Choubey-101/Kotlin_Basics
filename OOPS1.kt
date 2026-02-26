90d269c1e6311a2a45b9ff522347b85712abf8a4
Dipti Choubey <diptichoubey101@gmail.com>
2026-02-26 17:42:25 +0530

    Add files via upload

---
+++ OOPS1.kt
@@ -0,0 +1,65 @@
+//code including primary and seconding constructors
+//fun main(){
+//    val user = User(name = "Dipti")
+//    val friend =User(name = "Neel", lastName = "Dey")
+//    println("name:${user.name}")
+//    println("lastname:${user.lastName}")
+//    println("age:${user.age}")
+//
+//    println("name:${friend.name}")
+//    println("lastname:${friend.lastName}")
+//    println("age:${friend.age}")
+//}
+//class User(var name:String,var lastName:String,var age:Int){
+//    constructor(name:String):this(name,"lastName",0){
+//    println("2nd")
+//
+//}
+//    constructor(name:String,lastName:String):this(name,lastName,0){
+//    println("3rd")
+//    }
+//}
+
+
+
+
+
+//<----------------------------------------------------------------------->
+//making code concise
+//fun main(){
+//    val user = User("Dipti")
+//    val friend =User( "Neel", "Dey")
+//    println("First name:${user.firstName}")
+//    println("lastname:${user.lastName}")
+//    println("age:${user.age}")
+//
+//    println("First name:${friend.firstName}")
+//    println("lastname:${friend.lastName}")
+//    println("age:${friend.age}")
+//}
+//class User(var firstName:String,var lastName:String="LastName",var age:Int=0)
+
+
+
+
+
+//<--------------------------------------------------------------------->
+//making same code concise with 3 users
+//fun main(){
+//    val user = User("Dipti")
+//    val friend =User("Neel", "Dey")
+//    val user2=User("Diptanil","dey",20)
+//
+//    println("First name:${user.firstName}")
+//    println("lastname:${user.lastName}")
+//    println("age:${user.age}")
+//
+//    println("First name:${user2.firstName}")
+//    println("lastname:${user2.lastName}")
+//    println("age:${user2.age}")
+//
+//    println("First name:${friend.firstName}")
+//    println("lastname:${friend.lastName}")
+//    println("age:${friend.age}")
+//}
+//class User(var firstName:String,var lastName:String="LastName",var age:Int=0)
