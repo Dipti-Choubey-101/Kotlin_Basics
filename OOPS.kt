5dfdb706e8ef8513f38958a5a813e47429830d18
Dipti Choubey <diptichoubey101@gmail.com>
2026-02-28 20:08:18 +0530

    Add Kotlin getter and setter examples in User class
    Implemented custom getter and setter demonstrations for the User class in Kotlin.
    Included:
    1.Basic getter and setter usage
    2.Property reassignment using custom setter
    3.Concise getter syntax (get() = field)
    4.Default parameter values in constructor

--- OOPS.kt
+++ OOPS.kt
@@ -99,3 +99,89 @@ import kotlin.reflect.KProperty
 //        println("the car is stopped")
 //    }
 //}
+
+
+
+
+
+//GETTER AND SETTER
+//fun main(){
+//    val user = User("Dipti")
+//    val friend =User("Neel", "Dey")
+//
+//    println("First name:${user.firstName}")
+//    println("lastname:${user.lastName}")
+//    println("age:${user.age}")
+//
+//    println("First name:${friend.firstName}")
+//    println("lastname:${friend.lastName}")
+//    println("age:${friend.age}")
+//}
+//class User(firstName:String,var lastName:String="LastName",var age:Int=0) {
+//    var firstName=firstName
+//        get(){
+//            return "FirstName:$field"
+//        }
+//        set(value){
+//            println("$value was assigned to firstName property")
+//            field=value
+//        }
+//}
+
+
+
+//<---------------------------------------------------------------->
+//USING SETTER CODE BY REASSIGNING USER
+//fun main(){
+//    val user = User("Dipti")
+//    val friend =User("Neel", "Dey")
+//    user.firstName="Biva"
+//
+//    println(user.firstName)
+//    println(user.lastName)
+//    println(user.age)
+//
+//    println(friend.firstName)
+//    println(friend.lastName)
+//    println(friend.age)
+//}
+//class User(firstName:String,var lastName:String="LastName",var age:Int=0) {
+//    var firstName=firstName
+//        get(){
+//            return "FirstName:$field"
+//        }
+//        set(value){
+//            println("$value was assigned to firstName property")
+//            field=value
+//        }
+//}
+
+
+
+
+////using getter setter and concising code by writing getter code in one line
+//fun main(){
+//    val user = User("Dipti")
+//    val friend =User("Neel","Dey")
+//    user.firstName="Biva"
+//
+//    println(user.firstName)
+//    println(user.lastName)
+//    println(user.age)
+//
+//    println(friend.firstName)
+//    println(friend.lastName)
+//    println(friend.age)
+//}
+//class User(firstName:String,var lastName:String="LastName",var age:Int=0) {
+//    var firstName=firstName
+//        get()=field
+//
+//        set(value){
+//            println("$value was assigned to firstName property")
+//            field=value
+//        }
+//
+//}
+
+
