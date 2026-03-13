1ec127eb5a29d420108ae278b75acea41d27b64c
Dipti Choubey <diptichoubey101@gmail.com>
2026-03-13 12:41:59 +0530

    Update OOPS1.kt

--- OOPS1.kt
+++ OOPS1.kt
@@ -1,12 +1,12 @@
 //lateinit code:initializing value later but it won't be null
-//fun main(){
-//    val user = User("Dipti","Dey",19)
-//    user.favouriteMovie="3 idiots"
-//    println(user.favouriteMovie)
-//}
-//class User(var firstName:String,var lastName:String,var age:Int) {
-//    lateinit var favouriteMovie:String
-//}
+fun main(){
+   val user = User("Dipti","Dey",19)
+   user.favouriteMovie="3 idiots"
+   println(user.favouriteMovie)
+}
+class User(var firstName:String,var lastName:String,var age:Int) {
+   lateinit var favouriteMovie:String
+}
 
 
 
@@ -14,33 +14,33 @@
 
 //<----------------------------------------------------------------->
 //calculator class code
-//fun main(){
-//    val calculator=Calculator()
-//    val result=calculator.sum(5,3)
-//    println("result:$result")
-//}
-//class Calculator() {
-//    fun sum(a: Int, b: Int): Int {
-//        return a + b
-//    }
-//}
+fun main(){
+   val calculator=Calculator()
+   val result=calculator.sum(5,3)
+   println("result:$result")
+}
+class Calculator() {
+   fun sum(a: Int, b: Int): Int {
+       return a + b
+   }
+}
 
 
 
 
 //<-------------------------------------------------------------------->
 //HOW TO USE COMPANION OBJECT IN KOTLIN TO CALL A FUNCTION WITHOUT CREATING OBJECT OF CLASS
-//fun main(){
-//    val result = Calculator.sum(5, 6)
-//    println(result)
-//}
-//class Calculator(){
-//    companion object{
-//        fun sum(a:Int,b:Int):Int{
-//            return a+b
-//        }
-//    }
-//}
+fun main(){
+   val result = Calculator.sum(5, 6)
+   println(result)
+}
+class Calculator(){
+   companion object{
+       fun sum(a:Int,b:Int):Int{
+           return a+b
+       }
+   }
+}
 
 
 
@@ -48,16 +48,16 @@
 
 //<------------------------------------------------------------------------->
 //most imp code covering companion object,function calling and built-in constants like Int.MAX_VALUE
-//fun main(){
-// Int.MAX_VALUE
-//    val result =Calculator.sum(a=5,b=7)
-//    val max=Int.MAX_VALUE
-//    println(result)
-//}
-//class Calculator(){
-//    companion object{
-//        fun sum(a:Int,b:Int):Int{
-//            return a+b
-//        }
-//    }
-//}
+fun main(){
+Int.MAX_VALUE
+   val result =Calculator.sum(a=5,b=7)
+   val max=Int.MAX_VALUE
+   println(result)
+}
+class Calculator(){
+   companion object{
+       fun sum(a:Int,b:Int):Int{
+           return a+b
+       }
+   }
+}
