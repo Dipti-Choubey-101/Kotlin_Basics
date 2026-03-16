9b5c2f9b88ce601cb3cffd4805669786ac04fc09
Dipti Choubey <diptichoubey101@gmail.com>
2026-03-16 12:35:06 +0530

    Add coroutine completion handler using invokeOnCompletion
    Enhanced the coroutine async example by adding job completion handling.
    Included:
    1.Running parallel suspend functions using async
    2.Returning results with Deferred and retrieving them using await()
    3.Measuring execution time for concurrent tasks
    4.Using invokeOnCompletion to detect coroutine success or failure
    5.Logging completion status of the parent coroutine job
    
    This example demonstrates coroutine lifecycle handling and completion callbacks in Kotlin.

--- coroutines.kt
+++ coroutines.kt
@@ -161,3 +161,45 @@ private suspend fun getData2(threadName:String):String{
    println("Fake work2 finished:$threadName")
    return "Result 2"
 }
+
+
+
+
+fun main(){
+   println("Main program starts: ${Thread.currentThread().name}")
+   val start=System.currentTimeMillis()
+   val parentJob=CoroutineScope(Dispatchers.Default).launch {
+       val jobDeferred: Deferred<String> =async{
+           getData1(Thread.currentThread().name)
+       }
+       val jobDeferred2: Deferred<String> =async {
+           getData2(Thread.currentThread().name)
+       }
+       println(jobDeferred.await() + "\n${jobDeferred2.await()}")
+   }
+   runBlocking {
+       parentJob.join()
+   }
+   parentJob.invokeOnCompletion {
+       it?.let{
+           println("Parent job failed:${it.message}")
+       }?:println("Parent job success!")
+   }
+   println("Total time:${System.currentTimeMillis()-start}")
+
+   println("Main program ends: ${Thread.currentThread().name}")
+
+}
+private suspend fun getData1(threadName:String):String{
+   println("Fake work1 starts:$threadName")
+   delay(2000)
+   println("Fake work1 finished:$threadName")
+   return "Result 1"
+}
+
+private suspend fun getData2(threadName:String):String{
+   println("Fake work2 starts:$threadName")
+   delay(2000)
+   println("Fake work2 finished:$threadName")
+   return "Result 2"
+}
