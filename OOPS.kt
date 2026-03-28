86a25d93515ac85103857b1e98a46d8903cd8d56
Dipti Choubey <diptichoubey101@gmail.com>
2026-03-28 23:31:19 +0530

    Update OOPS.kt

--- OOPS.kt
+++ OOPS.kt
@@ -66,7 +66,6 @@ class Car(name:String,color:String,model:String,doors:Int){
 
 
 
-
 //<------------------------------------------------------>
 //its concise and easier to read with same output
 fun main(){
