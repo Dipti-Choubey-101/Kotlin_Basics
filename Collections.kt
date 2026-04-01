eefffb7fdba1b2ff597e004f0dc801bb3b36fa91
Dipti Choubey <diptichoubey101@gmail.com>
2026-04-01 20:52:56 +0530

    Update Collections.kt

--- Collections.kt
+++ Collections.kt
@@ -10,6 +10,7 @@ fun main(args:Array<String>) {
 data class User(val name: String)
 
 
+
 //<------------------------------------------------------------------->
 //maps
 fun main(args:Array<String>) {
