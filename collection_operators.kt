cd3a5cf3e3afb47f06eb8111593839cf2c325c0a
Dipti Choubey <diptichoubey101@gmail.com>
2026-04-04 22:12:37 +0530

    Update collection_operators.kt

--- collection_operators.kt
+++ collection_operators.kt
@@ -18,7 +18,6 @@ private fun searchElements(searchedElement: Int,numbers: MutableList<Int>):Int {
 
 
 
-
 //<------------------------------------------------------------------------->
 //Instead of checking every element, it divides the list in half each time
 //This is called Binary Search.
