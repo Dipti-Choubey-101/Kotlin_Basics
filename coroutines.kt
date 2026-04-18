5e0741dcd9ea72482cf5c6847638cd7cc0d59e21
Dipti Choubey <diptichoubey101@gmail.com>
2026-04-18 23:25:45 +0530

    Update coroutines.kt

--- coroutines.kt
+++ coroutines.kt
@@ -27,8 +27,7 @@ fun main(){//executed on main thread
    println("Main program ends: ${Thread.currentThread().name}")
 }
 
-
-//<-------------------------------------------------------------->
+//<------------------------------------------------------------->
 //using coroutine did this
 
 fun main(){
