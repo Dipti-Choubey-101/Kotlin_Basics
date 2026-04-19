29e5fa2aaa91e24d617954f9b7e587d784fa37b1
Dipti Choubey <diptichoubey101@gmail.com>
2026-04-19 23:16:04 +0530

    Update coroutines.kt

--- coroutines.kt
+++ coroutines.kt
@@ -19,7 +19,6 @@ import kotlinx.coroutines.Dispatchers.Main
 //using thread did this
 
 
-
 fun main(){//executed on main thread
    println("Main program starts: ${Thread.currentThread().name}")
    thread{
