0bddb7af868354e947e419461c7b1917010443ce
Dipti Choubey <diptichoubey101@gmail.com>
2026-04-29 23:33:20 +0530

    Update collection_operators.kt

--- collection_operators.kt
+++ collection_operators.kt
@@ -42,6 +42,7 @@ private fun searchElement(searchedElement: Int,numbers: MutableList<Int>):Int {
    return -1
 }
 
+
 //<------------------------------------------------------------------------>
 fun main() {
    println(searchElement(22,mutableListOf(1,12,22,422,642,246,64,23)))
