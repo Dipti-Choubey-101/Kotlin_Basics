8f5fabffb7013ada36b854b73ef12c842fa875d8
Dipti Choubey <diptichoubey101@gmail.com>
2026-05-02 23:25:08 +0530

    Update coroutines.kt

--- coroutines.kt
+++ coroutines.kt
@@ -30,7 +30,6 @@ fun main(){//executed on main thread
 //<------------------------------------------------------------->
 //using coroutine did this
 
-
 fun main(){
    println("Main program starts: ${Thread.currentThread().name}")
    CoroutineScope(Dispatchers.Default
