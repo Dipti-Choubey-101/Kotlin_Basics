37c84b11faeaebf1345b917a42033924f53ee4b0
Dipti Choubey <diptichoubey101@gmail.com>
2026-05-11 18:08:06 +0530

    Update Collections.kt

--- Collections.kt
+++ Collections.kt
@@ -109,10 +109,6 @@ fun main(args:Array<String>) {
    println(filteredIdx)
 }
 
-
-
-
-
 //<----------------------------------------------------------------->
     //filter not
 fun main(args:Array<String>) {
