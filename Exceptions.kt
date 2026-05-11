590052b1916867fda2b0f64ec62b2083a3a9c4c3
Dipti Choubey <diptichoubey101@gmail.com>
2026-05-11 18:02:26 +0530

    Add exception handling example using try-catch-finally
    Added a Kotlin example demonstrating exception handling.
    
    Included:
    1.Handling runtime errors such as division by zero and array index issues
    2.Using try-catch to catch ArithmeticException
    3.Returning a value from try-catch expression
    4.Using finally block for guaranteed execution

---
+++ Exceptions.kt
@@ -0,0 +1,26 @@
+//fun main(args:Array<String>) {
+//    val a = 5
+//    val b = 0
+//    println(a / b)//it gives error bcoz it is not possible
+//
+//    val number = arrayOf(2)
+//    println(number[1])//it also gives error
+//}
+
+
+//<------------------------------------------------------------>
+//so trying try and catch method
+//fun main(){
+//    val a=5
+//    val b=5
+//    val result=try{
+//        println(a/b)
+//        a/b
+//    }catch(e: ArithmeticException){
+//        println("you cant divide by 0:${e.message}")
+//        0
+//    }finally{
+//        println("divide by zero")
+//    }
+//    println(result)
+//}
