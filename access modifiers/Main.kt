960d1cafd2f142f765a0fdff3a4768422a76acbc
Dipti Choubey <diptichoubey101@gmail.com>
2026-05-16 17:17:42 +0530

    Add access modifier examples: public, private, and protected
    Added Kotlin examples demonstrating access modifiers and visibility control.
    
    Included:
    1.public modifier (default) with Car class accessible across packages
    2.private modifier in BankAccount to restrict direct access to balance
    3.Encapsulation using getter method (getBalance) and controlled updates (deposit)
    4.protected modifier in Vehicle class accessible only within subclasses
    5.Demonstrated inheritance-based access using Car subclass
    
    These examples illustrate data hiding, encapsulation, and controlled access in Kotlin OOP.

---
+++ access modifiers/Main.kt
@@ -0,0 +1,65 @@
+//public
+//package com.example.interfaces
+//import com.example.classes.Car
+//fun main() {
+//    val car = Car()
+//    println(car.brand)  // ✅ allowed
+//    println(car.color)  // ✅ allowed
+//}
+
+
+
+
+//<-------------------------------------------------------------->
+//private
+//fun main() {
+//    val acc = BankAccount()
+//
+//    // acc.balance -> Not allowed (private)
+//
+//    acc.deposit(1000)
+//    println("Current Balance: ${acc.getBalance()}")
+//}
+//
+//class BankAccount {
+//
+//    private var balance = 0
+//
+//    fun deposit(amount: Int) {
+//        if (amount > 0) {
+//            balance += amount
+//        }
+//    }
+//
+//    fun getBalance(): Int {
+//        return balance
+//    }
+//}
+
+
+
+
+
+
+//<----------------------------------------------------------->
+//protected
+//fun main() {
+//    val car = Car()
+//
+//    // car.speed ❌ Not allowed (protected)
+//    car.showSpeed()   // ✅ allowed
+//}
+//
+//open class Vehicle {
+//    protected val speed = 60
+//}
+//
+//class Car : Vehicle() {
+//    fun showSpeed() {
+//        println("Speed is: $speed")
+//    }
+//}
+
+
+
+
---
+++ access modifiers/classes.kt
@@ -0,0 +1,6 @@
+package com.example.classes
+
+class Car {
+    public var brand = "BMW"   // public (explicit)
+    public var color = "Red"          // public (implicit)
+}
