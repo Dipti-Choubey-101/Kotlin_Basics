5deeef40f9f43b11c73069858c14d5479a7277f1
Dipti Choubey <diptichoubey101@gmail.com>
2026-05-16 17:18:25 +0530

    Update Main.kt

--- access modifiers/Main.kt
+++ access modifiers/Main.kt
@@ -1,40 +1,40 @@
 //public
-//package com.example.interfaces
-//import com.example.classes.Car
-//fun main() {
-//    val car = Car()
-//    println(car.brand)  // ✅ allowed
-//    println(car.color)  // ✅ allowed
-//}
+package com.example.interfaces
+import com.example.classes.Car
+fun main() {
+   val car = Car()
+   println(car.brand)  // ✅ allowed
+   println(car.color)  // ✅ allowed
+}
 
 
 
 
 //<-------------------------------------------------------------->
 //private
-//fun main() {
-//    val acc = BankAccount()
-//
-//    // acc.balance -> Not allowed (private)
-//
-//    acc.deposit(1000)
-//    println("Current Balance: ${acc.getBalance()}")
-//}
-//
-//class BankAccount {
-//
-//    private var balance = 0
-//
-//    fun deposit(amount: Int) {
-//        if (amount > 0) {
-//            balance += amount
-//        }
-//    }
-//
-//    fun getBalance(): Int {
-//        return balance
-//    }
-//}
+fun main() {
+   val acc = BankAccount()
+
+   // acc.balance -> Not allowed (private)
+
+   acc.deposit(1000)
+   println("Current Balance: ${acc.getBalance()}")
+}
+
+class BankAccount {
+
+   private var balance = 0
+
+   fun deposit(amount: Int) {
+       if (amount > 0) {
+           balance += amount
+       }
+   }
+
+   fun getBalance(): Int {
+       return balance
+   }
+}
 
 
 
@@ -43,22 +43,22 @@
 
 //<----------------------------------------------------------->
 //protected
-//fun main() {
-//    val car = Car()
-//
-//    // car.speed ❌ Not allowed (protected)
-//    car.showSpeed()   // ✅ allowed
-//}
-//
-//open class Vehicle {
-//    protected val speed = 60
-//}
-//
-//class Car : Vehicle() {
-//    fun showSpeed() {
-//        println("Speed is: $speed")
-//    }
-//}
+fun main() {
+   val car = Car()
+
+   // car.speed ❌ Not allowed (protected)
+   car.showSpeed()   // ✅ allowed
+}
+
+open class Vehicle {
+   protected val speed = 60
+}
+
+class Car : Vehicle() {
+   fun showSpeed() {
+       println("Speed is: $speed")
+   }
+}
 
 
 
