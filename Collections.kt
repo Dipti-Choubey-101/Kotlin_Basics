30a46c7cfc77aa666c2e3d858bbeeefdf95c38c6
Dipti Choubey <diptichoubey101@gmail.com>
2026-07-05 23:36:18 +0530

    Update Collections.kt

--- Collections.kt
+++ Collections.kt
@@ -36,7 +36,6 @@ fun main(args:Array<String>) {
    println(numberSets[2][2])
 }
 
-
 //<------------------------------------------------------------------------->
 fun main(args:Array<String>) {
    val numberSets=listOf(setOf(1,2,3),setOf(4,5,6),setOf(7,8,9))
