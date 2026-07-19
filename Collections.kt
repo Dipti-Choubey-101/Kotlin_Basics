cab419251bc9192ea7fe93903db254b3d465bab9
Dipti Choubey <diptichoubey101@gmail.com>
2026-07-19 23:32:39 +0530

    Update Collections.kt

--- Collections.kt
+++ Collections.kt
@@ -9,7 +9,6 @@ fun main(args:Array<String>) {
    names.forEach{println(it.name)}
 }
 
-
 data class User(val name: String)
 //<------------------------------------------------------------------->
 //maps
