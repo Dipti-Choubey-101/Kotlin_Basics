c3edc9575e01ec8d7cea07eec0b392c044bce8ec
Dipti Choubey <diptichoubey101@gmail.com>
2026-08-24 23:34:41 +0530

    Update Collections.kt

--- Collections.kt
+++ Collections.kt
@@ -7,6 +7,8 @@ fun main(args:Array<String>) {
    val names=mutableSetOf<User>(user1,user2,user3,user4)
    names.forEach{println(it.name)}
 }
+
+
 data class User(val name: String)
 //<------------------------------------------------------------------->
 //maps
